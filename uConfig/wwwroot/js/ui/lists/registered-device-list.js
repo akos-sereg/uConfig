fced2aa2dd3bee8016dd5c184ac64afd995cfb3e
Akos Sereg <akos.sereg@gmail.com>
2022-02-10 20:36:52 +0100

    Device listing

--- uConfig/wwwroot/js/ui/lists/registered-device-list.js
+++ uConfig/wwwroot/js/ui/lists/registered-device-list.js
@@ -14,12 +14,14 @@ RegisteredDeviceList.prototype.refresh = function (devices) {
     let app = document.querySelector(this.placeholderId); // '#registered-device-list', #registered-device-template
     let listItem = document.querySelector(this.templateId);
     let list = document.createElement('div');
+    list.classList.add("registered_device_container");
 
     for (let registeredDevice of devices) {
-        let li = listItem.content.cloneNode(true).querySelector('div');
+        let record = listItem.content.cloneNode(true).querySelector('div');
 
-        li.textContent = registeredDevice.name;
-        list.append(li);
+        record.classList.add("registered_device_item");
+        record.querySelector('#device-card-title').textContent = registeredDevice.name;
+        list.append(record);
 
         app.append(list);
     }
